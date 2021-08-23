066f9bf83d621979ed19325e4a8554938d01f92d
abiagini <alessandro.biagini@yourbiz.it>
2021-08-23 16:50:52 +0200

    lezione 3, primo render

---
+++ 03 - Lezione/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //scene
+    const scene = new THREE.Scene()
+
+    //red cube object
+    const redCubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+    const redCubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    const redCubeMesh = new THREE.Mesh(redCubeGeometry, redCubeMaterial);
+    scene.add(redCubeMesh);
+
+    //camera
+    const sizes = {
+        width: 800,
+        height: 600
+    }
+    //parameters: field of view expressed in degrees, aspect ratio in pixels
+    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+    camera.position.z = 3;
+    camera.position.z = 3;
+    scene.add(camera);
+
+    // renderer
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector(".webgl")
+    })
+
+    renderer.setSize(sizes.width, sizes.height)
+
+    renderer.render(scene, camera)
+
+})
